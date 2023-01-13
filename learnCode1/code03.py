9c3cbaefe20d46726055bc90fc470f6c8a6d4d0a
李海飞 <1136014334@qq.com>
2023-01-14 00:38:26 +0800

    变量的创建与使用

---
+++ learnCode1/code03.py
@@ -0,0 +1,6 @@
+a = 10
+print(a)
+b = a
+print(b)
+a = 20
+print(a)
---
+++ learnCode1/code04.py
@@ -0,0 +1,19 @@
+a:int = 10
+print(type(a))
+b:float = 0.5
+print(type(b))
+c = True
+print(type(c))
+d = 'hello python'
+print(type(d))
+e = 'my name is "Andy"'
+print(type(e))
+f = '''my 'name' is "Andy"'''
+print(f)
+print(len(f))
+
+a1 = 'hello'
+a2 = ' world'
+b1 = 10
+print(a1 + a2)
+# print(a1 + b1)
