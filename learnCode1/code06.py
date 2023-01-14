b8f7a8f16d771ad30853d1982e70d7f8d84fad5e
李海飞 <1136014334@qq.com>
2023-01-14 16:43:37 +0800

    输入与输出

---
+++ learnCode1/code06.py
@@ -0,0 +1,22 @@
+a = 20
+print(f'a = {a+10}')
+
+# 通过控制台输入
+# num = input('请输入一个数字：')
+# print(f"输入的数字是： {num}")
+
+a = input("请输入一个整数：")  # input返回值是字符串
+b = input("请输入一个整数：")
+print(f'a + b = {a + b}')
+
+a = int(a)
+b = int(b)
+print(f'a + b = {a + b}')
+
+a = str(a)
+b = str(b)
+print(f'a + b = {a + b}')
+
+a = float(a)
+b = float(b)
+print(f'a + b = {a + b}')
